18c707cc38e1cbafa60b363eb0239f7e457eb433
TerekSW <tobias984@gmx.net>
2025-09-03 05:14:01 +0200

    Create db.py
    - create db.py for database interactions
    - define table for favorite recipes
    - save_favorite_recipes(recipe_id, details) to save favorited recipes into the database
    - get_favorite_recipes() to fetch stored recipes

---
+++ db.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import *
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "recipedb")
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
+
+metadata = MetaData()
+
+favorite_recipes = Table(
+    "favorite_recipes",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String),
+    Column("cuisine", String),
+    Column("dish_type", String),
+    Column("servings", Integer),
+    Column("source_url", String)
+)
+
+metadata.create_all(engine)
+
+def save_favorite_recipes(recipe_id, details):
+    with engine.connect() as con:
+        insert_stmt = favorite_recipes.insert().values(
+            id = recipe_id,
+            title = details["title"],
+            servings = details["servings"],
+            cuisine = details.get("cuisines", [""])[0] if details.get("cuisines") else None, # optionaler Wert
+            dish_type = details.get("dish_type", [""])[0] if details.get("dish_type") else None, # optionaler Wert
+            source_url = details.get("sourceUrl")
+        )
+        con.execute(insert_stmt)
+
+def get_favorite_recipes():
+    with engine.connect() as con:
+        result = con.execute(favorite_recipes.select())
+        return result.fetchall()
